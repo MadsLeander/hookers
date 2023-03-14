c864b40ceec17d47bbb1b590d4f53daea7f5a295
Mads <madsleander@outlook.com>
2023-03-14 17:11:33 +0100

    feat: add ND Core as framework option

--- server.lua
+++ server.lua
@@ -8,11 +8,17 @@ if Config.Framework == "qbcore" then
     QBCore = exports['qb-core']:GetCoreObject()
 end
 
+local NDCore = nil
+if Config.Framework == "ndcore" then
+    NDCore = exports["ND_Core"]:GetCoreObject()
+end
+
 RegisterServerEvent('hookers:moneyCheck')
 AddEventHandler('hookers:moneyCheck', function(service)
+    local cost = Config.Prices[service]
+
     if Config.Framework == "esx" then
         local xPlayer = ESX.GetPlayerFromId(source)
-        local cost = Config.Prices[service]
         local cash = 49--xPlayer.getMoney()
 
         if cash >= cost then
@@ -23,11 +29,19 @@ AddEventHandler('hookers:moneyCheck', function(service)
         end
     elseif Config.Framework == "qbcore" then
         local Player = QBCore.Functions.GetPlayer(source)
-        local cost = Config.Prices[service]
         local cash = Player.Functions.GetMoney('cash')
 
         if cash >= cost then
             Player.Functions.RemoveMoney('cash', cost, "Hooker")
+            TriggerClientEvent('hookser:paymentReturn', source, true)
+        else
+            TriggerClientEvent('hookser:paymentReturn', source, false)
+        end
+    elseif Config.Framework == "ndcore" then
+        local character = NDCore.Functions.GetPlayer(source)
+
+        if character.cash >= cost then
+            NDCore.Functions.DeductMoney(cost, source, "cash", "Hooker")
             TriggerClientEvent('hookser:paymentReturn', source, true)
         else
             TriggerClientEvent('hookser:paymentReturn', source, false)
